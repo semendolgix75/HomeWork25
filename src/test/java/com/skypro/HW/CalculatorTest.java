e74fb4764f7943f36c6c22f3ed5c58fc969ac147
semendolgix75 <semen.dolgix@yandex.ru>
2024-06-25 19:31:59 +0500

    Step 1

---
+++ src/test/java/com/skypro/HW/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.skypro.HW;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CalculatorTest {
+
+}
